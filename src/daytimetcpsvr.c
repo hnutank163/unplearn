e64157d6c72d76b29d71127e4f1f19e99e414fd2
tank <hnutank@163.com>
2015-07-16 18:30:31 +0800

    modify daytime svr

---
+++ src/daytimetcpsvr.c
@@ -0,0 +1,37 @@
+#include "unp.h"
+#include <time.h>
+
+int
+main(int argc, char ** argv)
+{
+    int listenfd, connfd;
+    int port = 13;
+    struct sockaddr_in svraddr, cliaddr;
+    char sendline[MAXLINE];
+
+    if( argc == 2 )
+        port = atoi(argv[1]);
+
+    if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) <= 0 )
+        err_sys("create socket error");
+
+    svraddr.sin_family = AF_INET;
+    svraddr.sin_port = htons(port);
+    svraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if( 0 != bind(listenfd, (struct sockaddr *)&svraddr, sizeof(svraddr )) )
+        err_sys("bind error");
+
+    if( 0 != listen(listenfd, 5) )
+        err_sys("listen error");
+
+    for(;;)
+    {
+        time_t ticks ;
+        socklen_t len = sizeof(cliaddr );
+        connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len );
+        ticks = time(NULL);
+        snprintf(buf, sizeof(buf), "%.24s\r\n", ctime(&ticks));
+
+}
+
