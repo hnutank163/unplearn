cfabc05a5f41f93019966bcca2f2162430a19a56
tank <hnutank@163.com>
2015-08-18 18:20:02 +0800

    simple http server

--- src/epoll/epoll_test.cpp
+++ src/epoll/epoll_test.cpp
@@ -1,6 +1,11 @@
+#include <stdio.h>
 #include <iostream>
+#include <string>
+#include <iterator>
+#include <fstream>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <errno.h>
@@ -12,16 +17,25 @@ const char *port="9999";
 int create_and_bind(const char *port);
 int make_socket_nonblocking(int fd);
 
+
+
 int
 main()
 {
+    ifstream in("index.html", ios::in);
     struct epoll_event event, events[MAXEVENTS];
     int fd, epfd;
     int ret;
-
+    string sbuf;
     fd = create_and_bind(port);
     if( fd==-1 )
         return 1;
+
+    if( 0 != listen(fd, 10) )
+    {
+        perror("listen");
+        return 1;
+    }
 
     ret = make_socket_nonblocking(fd);
     if(ret==-1)
@@ -43,16 +57,23 @@ main()
          return 1;
     }
 
+    if( in.is_open() )
+    {
+        istreambuf_iterator<char> beg(in), end;
+        sbuf += string(beg, end);
+        sbuf = string("HTTP/1.1 200 OK")+string("\r\n\r\n")+sbuf;
+        cout<<sbuf;
+    }
     memset(events, 0, sizeof(events));
     while(true)
     {
         int n, i;
         n = epoll_wait(epfd, events, MAXEVENTS, -1);
-
         for(i=0; i<n; i++)
         {
             uint32_t ev = events[i].events;
             int sock = events[i].data.fd;
+            char addrbuf[128];
             if( ev & EPOLLIN ) //read
             {
                 if( sock == fd )
@@ -64,17 +85,74 @@ main()
                          int connfd = accept(fd, (struct sockaddr *)&addr, &socklen);
                          if( connfd == -1 )
                          {
-                              if(errno == EAGAIN || errno == EINTR)
-                              {
-                                  usleep(1000);
-                                  continue;
-                              }
+                              if(errno == EAGAIN )
+                                break;
+                              perror("accept");
                               break;
                          }
-
-
+                        cout<<"recv connection from "<<inet_ntop(AF_INET, &addr.sin_addr,addrbuf, 16)<<" "<<ntohs(addr.sin_port)<<endl;
+                        make_socket_nonblocking(connfd);
+                        event.data.fd = connfd;
+                        event.events = EPOLLET|EPOLLIN;
+                        if( -1 == epoll_ctl(epfd,EPOLL_CTL_ADD, connfd, &event))
+                            perror("epoll_ctl add connfd");
                     }
                 }
+                else
+                {
+                     int err = 0;
+                     char buf[256];
+                     int n=0, nread;
+                     while(true)
+                     {
+                         nread = recv(sock, buf+n, 256, 0);
+                         if(nread > 0)
+                             n += nread;
+                         else if(nread == -1)
+                         {
+                            if( errno != EAGAIN )
+                                perror("read error");
+                            break;
+                         }
+                         else if(nread == 0)
+                         {
+                              err = 1;
+                              cout<<"client terminated\n";
+                              close(sock);
+                              epoll_ctl(epfd, EPOLL_CTL_DEL, sock, NULL);
+                              break;
+                         }
+                     }
+                     if( !err  )
+                     {
+                        buf[n] = 0;
+                        cout<<"recv "<<buf<<endl;
+                        send(sock, sbuf.c_str(), sbuf.size(), 0);
+                        close(sock);
+             /*           event.events = ev | EPOLLOUT | EPOLLIN;
+                        event.data.fd = sock;
+                        if( -1 == epoll_ctl(epfd, EPOLL_CTL_MOD, sock, &event) )
+                            perror("epoll_ctl_mod");*/
+                     }
+                }
+            }
+            else if( ev & EPOLLOUT )
+            {
+                int nwrite = 0, size=sbuf.size(), n = sbuf.size();
+                while( n>0 )
+                {
+                    nwrite = send(sock, sbuf.c_str()+size-n, n, 0);
+                    if( nwrite<n )
+                    {
+                        if( nwrite == -1 && errno != EAGAIN )
+                        {
+                            perror("write");
+                            break;
+                        }
+                    }
+                    n -= nwrite;
+                }
+                close(sock);
             }
         }
     }
@@ -92,7 +170,7 @@ int create_and_bind(const char * port)
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
-
+    inet_pton(AF_INET, "192.168.110.132", &hints.ai_addr);
     ret = getaddrinfo(NULL, port, &hints, &result);
     if( ret != 0 )
     {
@@ -105,11 +183,23 @@ int create_and_bind(const char * port)
         fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
         if( fd < 0 )
             continue;
+        else
+        {
+             int ops = 1;
+             if( -1 == setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (const void*)&ops, sizeof(int)) )
+                 perror("setsockopt");
+             printf("setsockopt success\n");
+        }
 
         ret = bind(fd, rp->ai_addr, rp->ai_addrlen);
         if( ret == 0 )
             break;
-        close(fd);
+        else
+        {
+            close(fd);
+            perror("bind");
+            return -1;
+        }
     }
 
     if( rp == NULL ) //mean bind failed
